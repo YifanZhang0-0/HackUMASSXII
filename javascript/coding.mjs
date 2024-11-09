30040eb76a47d9b38d96a634f46d6387eba5d9e9
Jun-L04 <LuJunyang04@gmail.com>
2024-11-08 22:23:08 -0500

    more boiler plate for encoding

--- javascript/coding.mjs
+++ javascript/coding.mjs
@@ -1,9 +1,31 @@
+packetHeader = 0xF1
+
+function updateByteArray(byteArray, data, length) {
+    newByteArray = new Uint8Array([...byteArray])
+    return newByteArray
+}
+
 export function encoding(func, ...params) {
-    byteArray = []
+    byteArray = new Uint8Array();
     // func is func obj
-    paramType = func.types
+    paramType = func.types // strings of the type itself
     ret = func.ret
     id = func.id
     funcName = func.name 
+
+    // type checking
+    for (let i = 0; i < params.length; i++) {
+        try {
+            typeof params[i] === paramType[i]
+        } catch (_) {
+            throw new Error("Type Mismatch in Parameters")
+        }
+    }
+
+    // encoding function parts into byteArray
+    
+
+    // encoding parameters into byteArray
+
     return btyeArray
 }

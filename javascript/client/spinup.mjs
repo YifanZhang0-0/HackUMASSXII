9d0c82ddce82be22e80766026a0186ed640be54e
andOrlando <bennettgillig@gmail.com>
2024-11-08 23:01:35 -0500

    do spinup and add void

--- javascript/client/spinup.mjs
+++ javascript/client/spinup.mjs
@@ -2,35 +2,38 @@ import * as net from "net"
 import * as fs from "fs"
 import { exec } from "child_process"
 import { PY, JS } from "../coding.mjs"
+import { Function } from "../classes.mjs"
 
 import { FDEF, INT, FLOAT, STRING, ARRAY, OBJECT } from "../magic.mjs"
 
-function setup_socket(library, socket_name) {
+async function setup_socket(library, socket_name) {
   if (fs.existsSync(socket_name)) fs.rmSync(socket_name)
 
-  const server = net.createServer((socket) => {
-    socket.on("readable", () => {
-      // first we have to get the functions
-      if (library.functions === undefined) {
+  const function_def_finished = new Promise((res, rej) => {
+    const server = net.createServer((socket) => {
+      socket.on("readable", () => {
+        // first we have to get the functions
+        if (library.functions === undefined) {
+          const head = new UInt8Array(socket.read(3))
+          // TODO: ASSERT HEAD IS 0xF2
+          const length = head[1] << 24 + head[2] << 16 + head[1] << 8 + head[2]
+          const data = new UInt8Array(socket.read(length))
+          get_functions(library, data, length)
+          return res()
+        }
+
+        // otherwise it's a return
         const head = new UInt8Array(socket.read(3))
-        // TODO: ASSERT HEAD IS 0xF2
         const length = head[1] << 4 + head[2]
         const data = new UInt8Array(socket.read(length))
-        get_functions(library, data, length)
-        return
-      }
-
-      //otherwise it's some type of data
-      const head = new UInt8Array(socket.read(3))
-      const length = head[1] << 4 + head[2]
-      const data = new UInt8Array(socket.read(length))
-    
+        // notify waitlist somehow
+      })
     })
-    // socket.on("end", () => console.log("end stream"))
-  })
 
-  server.listen(socket_name)
-  return server
+    server.listen(socket_name)
+  })
+  
+  await function_def_finished()
 }
 
 function run_server(type, file, socket_name) {
@@ -44,25 +47,36 @@ function run_server(type, file, socket_name) {
 }
 
 function get_functions(library, data, length) {
-  const id = data[0] << 4 + data[1]
-  const strlen = data[2] << 4 + data[3]
-  let s = ""
-  for (let i=4; i<4+strlen; i++) {
-    s += String.fromCharCode(data[i])
-  }
-
-  let types = []
-  while(i < length) {
-
-    
-    
-    data[i]
-    i++;
-  }
-
 
   library.functions = []
+  let sidx = 0;
+  while (sidx < length) {
+    sidx = get_function(library.functions, data, sidx)
+  }
 }
+
+
+function get_function(list, data, s) {
+  const id = data[s] << 8 + data[++s]
+  const slen = data[++s] << 8 + data[++s]
+  let name = ""
+  for (; s<slen; s++) {
+    name += String.fromCharCode(data[s])
+  }
+  const ret = data[++s]
+  const arglen = data[++s] << 8 + data[++s]
+  const types = []
+  for (; s < arglen; s++) {
+    types.push(data[s])
+  }
+  
+  list.push(new Function(types, ret, id, name))
+  return s
+  
+}
+
+
+
 
 
 

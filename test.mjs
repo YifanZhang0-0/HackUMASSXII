d9fac76d8d3f4a194abc899bb1d0853bedf9a363
andOrlando <bennettgillig@gmail.com>
2024-11-09 22:16:45 -0500

    Updates stuff

--- test.mjs
+++ test.mjs
@@ -1,13 +1,10 @@
-
 import { loadPY } from "./unicallJS/client/load.mjs"
 const obj = await loadPY("test.py")
-console.info = () => {};
-// console.log(obj.addfive)
+console.info = () => {}; // silence errors
+
 console.log(await obj.addfive(5))
 console.log(await obj.addfive(10))
-
 console.log(await obj.addtoarray(2.5, [0, 2, 3]))
-
 
 obj.close()
 
